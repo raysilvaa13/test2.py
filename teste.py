299458f26ab77c2d7cea4b0eb1a24571c46d6c2f
raysilvaa13 <rayanaoficial13@gmail.com>
2024-06-12 13:52:57 -0300

    operadores aritméticos no python

---
+++ teste.py
@@ -0,0 +1,28 @@
+adicao = 10+10
+print('adição', adicao)
+
+subtracao = 10-5
+print('subtração', subtracao)
+
+multiplicacao =10*10
+print('multiplicação', multiplicacao)
+
+divisao = 10/3 #float
+print('divisão', divisao)
+
+divisao_inteira = 10 // 3
+print('divisão_inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
+
+      
